e0e1751af5971d32569a1ff4656a07e33cda7793
Charley Oliveira <charleycesar@gmail.com>
2020-10-31 17:08:19 -0300

    feat(modules): add home module

---
+++ src/modules/home/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Box from '@UI/Box'
+import Text from '@UI/Text'
+
+const Home = () => {
+    return (
+        <Box>
+            <Text>Teste</Text>
+        </Box>
+    )
+}
+
+export default Home
