ac339d641956550de8020e92a2ca0225d910259b
Charley Oliveira <charleycesar@gmail.com>
2020-10-31 16:56:18 -0300

    feat(UI): add component Box with animation

---
+++ src/components/UI/Box/index.js
@@ -0,0 +1,212 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import * as Animatable from 'react-native-animatable'
+import { useTheme } from '@styles/hooks'
+
+import { propTypes, defaultProps } from './propTypes'
+
+const TheBox = (props) => {
+    const {
+        m,
+        mt,
+        mr,
+        mb,
+        ml,
+        mx,
+        my,
+        p,
+        pt,
+        pr,
+        pb,
+        pl,
+        px,
+        py,
+        display,
+        alignItems,
+        justifyContent,
+        flexGrow,
+        flexShrink,
+        direction,
+        bgcolor,
+        fullWidth,
+        width,
+        height,
+        children,
+        boxShadow,
+        border,
+        wrap,
+        borderRadius,
+        fullscreen,
+        absolutePosition,
+        animation,
+    } = props
+
+    const theme = useTheme()
+
+    if (display === 'none') return null
+
+    const styleObj = {
+        flexGrow,
+        flexShrink,
+        height,
+        margin: m && theme.space.default * m,
+        marginTop: mt && theme.space.default * mt,
+        marginRight: mr && theme.space.default * mr,
+        marginBottom: mb && theme.space.default * mb,
+        marginLeft: ml && theme.space.default * ml,
+        marginVertical: my && theme.space.default * my,
+        marginHorizontl: mx && theme.space.default * mx,
+
+        padding: p && theme.space.default * p,
+        paddingTop: pt && theme.space.default * pt,
+        paddingRight: pr && theme.space.default * pr,
+        paddingBottom: pb && theme.space.default * pb,
+        paddingLeft: pl && theme.space.default * pl,
+        paddingVertical: py && theme.space.default * py,
+        paddingHorizontal: px && theme.space.default * px,
+
+        backgroundColor: bgcolor,
+        borderRadius: borderRadius ? borderRadius : 0,
+    }
+    if (!fullscreen && !fullWidth && width) {
+        styleObj.width = width
+    }
+
+    if (wrap) {
+        styleObj.flexWarp = 'wrap'
+    }
+
+    if (border) {
+        styleObj.borderRadius = border.radius ? border.radius : 0
+        styleObj.borderWidth = border.width ? border.width : 0
+        styleObj.borderTopWidth = border.borderTopWidth
+            ? border.borderTopWidth
+            : styleObj.borderWidth
+        styleObj.borderBottomWidth = border.borderBottomWidth
+            ? border.borderBottomWidth
+            : styleObj.borderWidth
+        styleObj.borderColor = border.color
+            ? theme.palette[border.color].main
+            : theme.palette['primary'].main
+    }
+
+    if (absolutePosition) {
+        absolutePosition.position = 'absolute'
+    }
+
+    return (
+        <Animatable.View
+            style={[
+                localStyle.Box,
+                localStyle[`align-${alignItems}`],
+                localStyle[`justify-${justifyContent}`],
+                localStyle[`direction-${direction}`],
+                localStyle[`box-shadow-${boxShadow}`],
+                fullWidth && localStyle.fullWidth,
+                display === 'view' && localStyle.view,
+                display === 'flex' && localStyle.flex,
+                fullscreen && localStyle.fullscreen,
+                styleObj,
+                absolutePosition,
+            ]}
+            animation={animation}>
+            {children}
+        </Animatable.View>
+    )
+}
+
+TheBox.propTypes = {
+    ...propTypes,
+}
+
+TheBox.defaultProps = {
+    ...defaultProps,
+}
+
+const localStyle = StyleSheet.create({
+    Box: {
+        width: 'auto',
+        zIndex: 0,
+    },
+    fullWidth: {
+        width: '100%',
+    },
+    view: {
+        flexShrink: 1,
+        flexGrow: 0,
+    },
+    flex: {
+        flex: 1,
+    },
+    'align-flex-start': {
+        alignItems: 'flex-start',
+    },
+    'align-flex-end': {
+        alignItems: 'flex-end',
+    },
+    'align-center': {
+        alignItems: 'center',
+    },
+    'justify-flex-start': {
+        justifyContent: 'flex-start',
+    },
+    'justify-flex-end': {
+        justifyContent: 'flex-end',
+    },
+    'justify-center': {
+        justifyContent: 'center',
+    },
+    'justify-space-around': {
+        justifyContent: 'space-around',
+    },
+    'justify-space-between': {
+        justifyContent: 'space-between',
+    },
+    'direction-row': {
+        flexDirection: 'row',
+    },
+    'direction-column': {
+        flexDirection: 'column',
+    },
+    'box-shadow-0': {},
+    'box-shadow-1': {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.18,
+        shadowRadius: 1.0,
+        elevation: 1,
+    },
+    'box-shadow-2': {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        elevation: 2,
+    },
+    'box-shadow-3': {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        elevation: 3,
+    },
+    fullscreen: {
+        flex: 1,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        height: 400,
+    },
+})
+
+export default React.memo(TheBox)
---
+++ src/components/UI/Box/propTypes.js
@@ -0,0 +1,81 @@
+import PropTypes from 'prop-types'
+
+export const propTypes = {
+    bgcolor: PropTypes.string,
+    display: PropTypes.oneOf(['none', 'view', 'flex']),
+    direction: PropTypes.oneOf(['row', 'column']),
+    alignItems: PropTypes.oneOf(['flex-start', 'flex-end', 'center']),
+    flexGrow: PropTypes.number,
+    flexShrink: PropTypes.number,
+    justifyContent: PropTypes.oneOf([
+        'flex-start',
+        'flex-end',
+        'center',
+        'space-around',
+        'space-between',
+        'space-evenly',
+    ]),
+
+    width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    fullWidth: PropTypes.bool,
+
+    m: PropTypes.number,
+    mt: PropTypes.number,
+    mr: PropTypes.number,
+    mb: PropTypes.number,
+    ml: PropTypes.number,
+
+    mx: PropTypes.number,
+    my: PropTypes.number,
+
+    p: PropTypes.number,
+    pt: PropTypes.number,
+    pr: PropTypes.number,
+    pb: PropTypes.number,
+    pl: PropTypes.number,
+
+    px: PropTypes.number,
+    py: PropTypes.number,
+
+    boxShadow: PropTypes.oneOf([0, 1, 2, 3]),
+
+    borderRadius: PropTypes.number,
+    absolutePosition: PropTypes.object,
+    animation: PropTypes.string,
+}
+
+export const defaultProps = {
+    bgcolor: undefined,
+    display: 'view',
+    alignItems: 'flex-start',
+    flexGrow: undefined,
+    flexShrink: undefined,
+    direction: 'column',
+    fullWidth: true,
+    width: undefined,
+    height: undefined,
+    absolutePosition: undefined,
+
+    m: undefined,
+    mt: undefined,
+    mr: undefined,
+    mb: undefined,
+    ml: undefined,
+
+    mx: undefined,
+    my: undefined,
+
+    p: undefined,
+    pt: undefined,
+    pr: undefined,
+    pb: undefined,
+    pl: undefined,
+
+    px: undefined,
+    py: undefined,
+
+    boxShadow: 0,
+    borderRadius: 0,
+    animation: '',
+}
