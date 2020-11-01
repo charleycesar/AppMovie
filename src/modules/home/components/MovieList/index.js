1287daae5d3993aeb960615fa50ac4d8d07604ca
Charley Oliveira <charleycesar@gmail.com>
2020-11-01 14:07:44 -0300

    refactor(modules): add movieList component

---
+++ src/modules/home/components/MovieList/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Box from '@UI/Box'
+import Text from '@UI/Text'
+import MovieCard from '@modules/home/components/MovieCard'
+import { FlatList } from 'react-native'
+
+const MovieList = ({ title, movies }) => (
+    <Box animation={'slideInRight'}>
+        <Box pb={1}>
+            <Text variant="h4">{title}</Text>
+        </Box>
+        <FlatList
+            data={movies}
+            style={{
+                width: '100%',
+            }}
+            ItemSeparatorComponent={() => <Box fullWidth={false} width={20} />}
+            horizontal
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => {
+                return (
+                    <MovieCard
+                        data={{
+                            title: item.title,
+                            uri: `/w200${item.poster_path}`,
+                            release_date: item.release_date,
+                        }}
+                        key={item.title}
+                    />
+                )
+            }}
+        />
+    </Box>
+)
+
+export default MovieList
--- src/modules/home/screens/Home.js
+++ src/modules/home/screens/Home.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import Box from '@UI/Box'
-import Text from '@UI/Text'
 import Screen from '@UI/Screen'
 import HeaderNavigation from '@modules/home/components/HeaderNavigation'
-import MovieCard from '@modules/home/components/MovieCard'
-import { FlatList } from 'react-native'
 import useMovies from '@modules/home/hooks/useMovies'
+import MovieList from '@modules/home/components/MovieList'
 
 const Home = () => {
     const { getMovies, movies } = useMovies()
@@ -18,34 +15,7 @@ const Home = () => {
     return (
         <Screen fullScreen>
             <HeaderNavigation options={options} />
-            <Box animation={'slideInRight'}>
-                <Box pb={1}>
-                    <Text variant="h4">{movies.title}</Text>
-                </Box>
-                <FlatList
-                    data={movies.results}
-                    style={{
-                        width: '100%',
-                    }}
-                    ItemSeparatorComponent={() => (
-                        <Box fullWidth={false} width={20} />
-                    )}
-                    horizontal
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={({ item }) => {
-                        return (
-                            <MovieCard
-                                data={{
-                                    title: item.title,
-                                    uri: `/w200${item.poster_path}`,
-                                    release_date: item.release_date,
-                                }}
-                                key={item.title}
-                            />
-                        )
-                    }}
-                />
-            </Box>
+            <MovieList title={movies.title} movies={movies.results} />
         </Screen>
     )
 }
