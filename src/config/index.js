9e80959e5d894745d9e35e189450a2f028fc36e6
Charley Oliveira <charleycesar@gmail.com>
2020-11-01 16:03:45 -0300

    feat(api): change to v3

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const Config = {
-    API_URL: 'https://api.themoviedb.org/4/',
+    API_URL: 'https://api.themoviedb.org/3/',
     IMAGE_BASE_URL: 'https://image.tmdb.org/t/p',
     TOKEN_TMDB:
         'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMzAwYjg3NGQ1NTExNjBmYWE5ZmYzZmFiYTYzMzE3NSIsInN1YiI6IjVmOWUxMjQ5MTk2NzU3MDAzOGEwMzEzZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._tDiQpTYW_QwW4eqjTXvGqpiq-kunV0sB9NDB1Qw1IQ',
