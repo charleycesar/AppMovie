2593dd9f338920c742fd05ec66c0b9d264057f47
Charley Oliveira <charleycesar@gmail.com>
2020-11-01 16:03:10 -0300

    refactor(core): remove files

--- src/services/Movie/FetchOne.js
+++
@@ -1,9 +0,0 @@
-import api, { handleError } from '@/services'
-
-export default async (movieId) => {
-    if (!movieId) {
-        return handleError({ message: 'Movie ID is required' })
-    }
-    const response = await api.get(`movie/${movieId}`)
-    return response.data
-}
