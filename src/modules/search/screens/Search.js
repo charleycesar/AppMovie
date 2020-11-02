361472221e18d6ed6f89db9930a6f9e66604b834
Charley Oliveira <charleycesar@gmail.com>
2020-11-01 23:53:22 -0300

    feat(modules): add search module

---
+++ src/modules/search/screens/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Screen from '@UI/Screen'
+import Box from '@UI/Box'
+import Text from '@UI/Text'
+
+const Search = () => {
+    return (
+        <Screen fullScreen>
+            <Box>
+                <Text>Busca</Text>
+            </Box>
+        </Screen>
+    )
+}
+
+export default Search
