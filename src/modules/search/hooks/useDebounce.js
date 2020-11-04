1350cb0ce05f151a90d80116ba9fe84b5e17212b
Charley Oliveira <charleycesar@gmail.com>
2020-11-04 00:18:42 -0300

    feat(modules): add hooks useDebounce

---
+++ src/modules/search/hooks/useDebounce.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = React.useState(value)
+
+    React.useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value)
+        }, delay)
+
+        return () => {
+            clearTimeout(handler)
+        }
+    }, [value])
+
+    return debouncedValue
+}
+export default useDebounce
