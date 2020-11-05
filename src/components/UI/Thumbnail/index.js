d91a5c7c11cca1eaed58c9b3c450876387589813
Charley Oliveira <charleycesar@gmail.com>
2020-11-05 04:06:55 -0300

    refactor(UI): add placeholder image when image is loading

--- src/components/UI/Thumbnail/index.js
+++ src/components/UI/Thumbnail/index.js
@@ -2,6 +2,7 @@ import { propTypes, defaultProps } from './propTypes'
 import React from 'react'
 import FastImage from 'react-native-fast-image'
 import { Config } from '@/config'
+import { ActivityIndicator } from 'react-native'
 
 const Thumbnail = ({
     uri,
@@ -10,20 +11,30 @@ const Thumbnail = ({
     borderRadius,
     noBorderRadius,
     usePrefix,
-}) => (
-    <FastImage
-        style={{
-            width,
-            height,
-            borderRadius: noBorderRadius === true ? 0 : borderRadius,
-        }}
-        source={{
-            uri: usePrefix ? `${Config.IMAGE_BASE_URL}${uri}` : uri,
-            priority: FastImage.priority.normal,
-        }}
-        resizeMode={FastImage.resizeMode.contain}
-    />
-)
+}) => {
+    const [loading, setLoading] = React.useState(false)
+    const fullUrl = usePrefix ? `${Config.IMAGE_BASE_URL}${uri}` : uri
+    return (
+        <FastImage
+            style={{
+                width,
+                height,
+                borderRadius: noBorderRadius === true ? 0 : borderRadius,
+            }}
+            source={{
+                uri: fullUrl,
+                priority: FastImage.priority.normal,
+            }}
+            fallback
+            onLoadStart={() => setLoading(true)}
+            onLoadEnd={() => setLoading(false)}
+            defaultSource={require('@assets/images/placeholder.png')}
+            onError={(response) => console.log('Deu erro')}
+            resizeMode={FastImage.resizeMode.contain}>
+            <ActivityIndicator animating={loading} />
+        </FastImage>
+    )
+}
 
 Thumbnail.propTypes = {
     ...propTypes,
