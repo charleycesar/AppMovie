3226579ee721f8863cec433493c18e9c09912629
Charley Oliveira <charleycesar@gmail.com>
2020-11-06 01:04:17 -0300

    feat(core): add SplashScreen in IOS

--- src/App.js
+++ src/App.js
@@ -6,19 +6,26 @@ import { store, persistor } from '@/store'
 import { StatusBar } from 'react-native'
 import Navigator from '@router/Navigator'
 import ThemeProvider from '@styles/ThemeProvider'
+import SplashScreen from 'react-native-splash-screen'
 
-const App = () => (
-    <ThemeProvider>
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <StatusBar
-                    backgroundColor={'transparent'}
-                    barStyle="dark-content"
-                />
-                <Navigator />
-            </PersistGate>
-        </Provider>
-    </ThemeProvider>
-)
+const App = () => {
+    React.useEffect(() => {
+        SplashScreen.hide()
+    }, [])
+
+    return (
+        <ThemeProvider>
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <StatusBar
+                        backgroundColor={'transparent'}
+                        barStyle="dark-content"
+                    />
+                    <Navigator />
+                </PersistGate>
+            </Provider>
+        </ThemeProvider>
+    )
+}
 
 export default App
